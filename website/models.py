bee9b1bfdd221978339436f52e473b2744462942
DanielCosmin <dciolacu87@yahoo.com>
2024-08-15 21:46:10 +0300

    updated db schema

--- website/models.py
+++ website/models.py
@@ -29,11 +29,28 @@ class VotingApp(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(50), nullable = False)
     owner = db.relationship("Admin", back_populates="voting_apps")
+    voting_sessions = db.relationship("VotingSession", back_populates = "owner")
 
     """
     here I need to add every attribute than we want to make configurable by the user
+    
+    * voters_number 
+    * background color
+    * background picture
+    * 
+
     """
 
-class VotingSession(db.Model): ...
+class VotingSession(db.Model): 
+    id = db.Column(db.Integer, primary_key = True)
+    motion = db.Column(db.String(), nullable = False)
+    status = db.Column(db.String(), nullable = False)
+    owner = db.relationship("VotingApp", back_populates = "voting_sessions")
 
-class Passwords(db.Model): ...
+
+class PasswordLot(db.Model): 
+    id = db.Column(db.Integer, primary_key = True)
+    passwords_number = db.Column(db.Integer, nullable = False)
+
+
+class Password(db.Model): ...
